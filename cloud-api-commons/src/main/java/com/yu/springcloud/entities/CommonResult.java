440d81e44dcf318415942dc292bc68e2ea64e27d
yuxiaohe <195518974@qq.com>
2020-07-09 17:12:59 +0800

    idea_commit

--- cloud-api-commons/src/main/java/com/yu/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/yu/springcloud/entities/CommonResult.java
@@ -15,7 +15,7 @@ public class CommonResult<T> implements Serializable
     private String  message;
     private T       data;
 
-
+//测试githup
     public CommonResult(Integer code, String message)
     {
         this(code,message,null);
